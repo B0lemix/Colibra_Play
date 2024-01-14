4aa353efe216a964055810a3b79380de75d61694
B0lemix <109000296+B0lemix@users.noreply.github.com>
2024-01-14 14:13:45 +0100

    Anadido base de datos con info de prueba usando MongoDB y esquema de datos usando Prisma.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   integrations: [tailwind()],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
