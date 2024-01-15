bac8126647496a75b66ddb41e5e53d8d590ea15a
B0lemix <109000296+B0lemix@users.noreply.github.com>
2024-01-15 21:44:49 +0100

    Anadido base de datos con info de prueba usando MongoDB y esquema de datos usando Prisma.

--- src/pages/api/random.ts
+++ src/pages/api/random.ts
@@ -1,14 +1,14 @@
-import prismadb from "../../../lib/prismadb";
+/* import prismadb from "../../../lib/prismadb"; */
 import type { APIRoute } from "astro";
  
 export const GET: APIRoute = async  ({  params, request  })  =>  {
- const contentCount = await prismadb.content.count();
+/*  const contentCount = await prismadb.content.count();
  const randomIndex = Math.floor(Math.random() * contentCount);
 
   const randomContent = await prismadb.content.findMany({
     take: 1,
     skip: randomIndex,
-  });
+  }); */
 /*   if (contentCount === 0) {
     return new Response(null, {
       status: 404,
@@ -16,8 +16,8 @@ export const GET: APIRoute = async  ({  params, request  })  =>  {
     });
   } */
 
-
- return new Response(JSON.stringify(randomContent[0]), {
+/* JSON.stringify(randomContent[0]) */
+ return new Response("hello", {
     status: 200,
     headers: {
       "Content-Type": "application/json",
