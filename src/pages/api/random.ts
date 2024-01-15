4fae5719bc5eba175a515a337323a0e971952b65
B0lemix <109000296+B0lemix@users.noreply.github.com>
2024-01-15 20:32:22 +0100

    Anadido base de datos con info de prueba usando MongoDB y esquema de datos usando Prisma.

--- src/pages/api/random.ts
+++ src/pages/api/random.ts
@@ -1,4 +1,4 @@
-/* import prismadb from "../../../lib/prismadb";
+import prismadb from "../../../lib/prismadb";
 export async function GET({  params, request  }) {
   const contentCount = await prismadb.content.count();
   const randomIndex = Math.floor(Math.random() * contentCount);
@@ -22,4 +22,3 @@ export async function GET({  params, request  }) {
     },
   });
 }
- */
