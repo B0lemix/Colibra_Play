d02104cd0ae6ec87a799b6c1f51636135d3d5c6e
B0lemix <109000296+B0lemix@users.noreply.github.com>
2024-01-15 20:19:34 +0100

    Anadido base de datos con info de prueba usando MongoDB y esquema de datos usando Prisma.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   integrations: [tailwind()],
-  adapter: node({
-    mode:"standalone"
-  })
 });
